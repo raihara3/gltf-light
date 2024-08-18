32d57aa4a13444145f33568fdd7e7cd29c6b426e
raihara3 <raihara3@gmail.com>
2024-08-18 15:47:54 +0900

    add global text style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Sometype_Mono } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const sometypeMono = Sometype_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={sometypeMono.className}>{children}</body>
     </html>
   );
 }
