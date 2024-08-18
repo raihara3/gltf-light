6eed8d126e7df509bc03911626ea7eef8d833dde
raihara3 <raihara3@gmail.com>
2024-08-18 16:17:19 +0900

    add layouts.module.scss

---
+++ app/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside>
+      Sidebar
+    </aside>
+  );
+}
+
+export default memo(Sidebar);
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
+import { Fragment } from "react";
 import Image from "next/image";
 
 // style
 import "./styles/global.scss";
+import styles from "./styles/layouts.module.scss"
+
+// components
+import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
-    <main>
-      GLTF Light
-    </main>
+    <Fragment>
+      <main className={styles.layoutTwoColumn}>
+        <div className={styles.layoutColumnLeft}>
+          <Sidebar />
+        </div>
+        <div>
+          Uploader
+        </div>
+      </main>
+    </Fragment>
   );
 }
