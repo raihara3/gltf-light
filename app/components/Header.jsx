9961849cb8c59947824b6c7a0c49d39773858d60
raihara3 <raihara3@gmail.com>
2024-08-19 17:02:58 +0900

    add header link

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,8 +7,13 @@ import styles from "../styles/components/header.module.scss"
 const Header = () => {
   return (
     <header className={styles.header}>
-      GLTF Light
-      <img src="/images/rocket.png" alt="rocket" width="30" height="30" />
+      <div className={styles.left}>
+        GLTF Light
+        <img src="/images/rocket.png" alt="rocket" width="30" height="30" />
+      </div>
+      <div className={styles.right}>
+        <a href="https://x.com/raihara3" target='_blank'>raihara3</a>
+      </div>
     </header>
   )
 }
