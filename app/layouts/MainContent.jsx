1fc6ca3cdf7bb64e6dda94613a773b8840bba013
raihara3 <raihara3@gmail.com>
2025-02-17 00:56:59 +0900

    add resize texture

--- app/layouts/MainContent.jsx
+++ app/layouts/MainContent.jsx
@@ -1,11 +1,9 @@
 // lib
-import { memo, Fragment } from 'react';
-import { useRecoilValue } from 'recoil';
+import { memo, Fragment, useState } from "react";
+import { useRecoilValue } from "recoil";
 
 // states
-import {
-  filePathState,
-} from "../state/atoms/Upload3DModelAtom";
+import { filePathState } from "../state/atoms/Upload3DModelAtom";
 
 // component
 import Viewer from "../components/Viewer";
@@ -13,28 +11,37 @@ import Logbox from "../components/Logbox";
 import TextureResizeModal from "../components/TextureResizeModal";
 
 // styles
-import styles from "../styles/layouts/mainContent.module.scss"
+import styles from "../styles/layouts/mainContent.module.scss";
 
 const MainContent = () => {
   const filePath = useRecoilValue(filePathState);
+  const [currentResizeTexture, setCurrentResizeTexture] = useState(null);
 
   return (
     <div className={styles.wrap}>
       {filePath ? (
         <Fragment>
-          <Viewer />
+          <Viewer currentResizeTexture={currentResizeTexture} />
           <Logbox />
-          <TextureResizeModal />
+          <TextureResizeModal
+            setCurrentResizeTexture={setCurrentResizeTexture}
+          />
         </Fragment>
       ) : (
         <div className={styles.innerBox}>
-          This is lightweight service specialized in GLTF.<br />
-          Displays warning on using glb in WebAR.<br/>
-          <img className={styles.symbolMark} src="/images/rocket.png" alt="rocket" />
+          This is lightweight service specialized in GLTF.
+          <br />
+          Displays warning on using glb in WebAR.
+          <br />
+          <img
+            className={styles.symbolMark}
+            src="/images/rocket.png"
+            alt="rocket"
+          />
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default memo(MainContent);
